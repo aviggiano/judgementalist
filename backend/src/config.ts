89aadfe38048d148cee4ef40fe868fb7335d1296
Prathamesh Raut <pratham.raut07@gmail.com>
2023-06-20 02:07:23 +0530

    Moved db.json to .data directory

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const path = require('node:path')
+
 const config = {
   port: process.env.PORT || 4000,
-  db: process.env.DB || "db.json",
+  db: process.env.DB || path.join(".data", "db.json"),
 };
 
 export default config;
--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,5 +1,6 @@
 import fs from "fs/promises";
 import config from "../config";
+import * as path from 'path';
 
 let connected = false;
 let data: Record<string, any> = {};
@@ -7,6 +8,8 @@ let data: Record<string, any> = {};
 export async function connect(): Promise<void> {
   if (connected) return;
   try {
+    createDirectoryAndFileIfNotExist(config.db);
+
     const content = await fs.readFile(config.db);
     data = JSON.parse(content.toString());
     connected = true;
@@ -22,4 +25,47 @@ export async function set(key: string, value: any): Promise<any> {
   data[key] = value;
   await fs.writeFile(config.db, JSON.stringify(data));
   return data[key];
+}
+
+async function createDirectoryAndFileIfNotExist(filePath: string) {
+  try {
+    const directoryPath = path.dirname(filePath);
+    // Check if the directory exists
+    if (!await directoryExists(directoryPath)) {
+      // Create the directory
+      await fs.mkdir(directoryPath, { recursive: true });
+    }
+
+    // Check if the file exists
+    if (!await fileExists(filePath)) {
+      // Create the file
+      await fs.writeFile(filePath, "");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function directoryExists(directoryPath: string): Promise<boolean> {
+  try {
+    const directoryStats = await fs.stat(directoryPath);
+    return directoryStats.isDirectory();
+  } catch (error: any) {
+    if (error?.code === "ENOENT") {
+      return false;
+    }
+    throw error;
+  }
+}
+
+async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    const fileStats = await fs.stat(filePath);
+    return fileStats.isFile();
+  } catch (error: any) {
+    if (error?.code === "ENOENT") {
+      return false;
+    }
+    throw error;
+  }
 }
